49ca7cba1fc0d9b7e2aff9cde55c6795266f128e
JoshuaFaaui <joshuafaaui@gmail.com>
2022-09-03 13:17:23 +1200

    real3 saturday

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
+const usermess = document.getElementById("message")
+const userem = document.getElementById("email")
+const modal = document.querySelector('.customalert')
 
 function submitter(clicked) {
     if (usermess.value.length && userem.value.length > 0) {
         modal.style.display = 'block';
     }
-    
 }
 function exitalert(clicked) {
     modal.style.display = 'none'
