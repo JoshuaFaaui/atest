f5a2adbd1d472ccb19d31072586d52fffad8ac3a
JoshuaFaaui <joshuafaaui@gmail.com>
2022-09-03 19:44:41 +1200

    Saturdays publish

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
+
+// --------------------------\/the custom alert\/-----------------------
+
 const usermess = document.getElementById("message")
 const userem = document.getElementById("email")
 const modal = document.querySelector('.customalert')
-
+const usern = document.getElementById("name")
+const cont = document.querySelector('.cont')
+const cont1 = document.getElementById("cont1")
+//---------function runs on submition\/
 function submitter(clicked) {
     if (usermess.value.length && userem.value.length > 0) {
-        modal.style.display = 'block';
+        if(usern.value.length > 0){
+            modal.style.display = 'block';
+        }  
     }
+    // a for loop to get rid of unecessary symbols in the users name\/
+    for (let i = 0; i < usern.value.length; i++){
+        const x = usern.value[i]
+        switch (x){
+            case '~':
+                continue;
+            case '!':
+                continue;
+            case '@':
+                continue;
+            case '#':
+                continue;
+            case '$':
+                continue;
+            case '%':
+                continue;
+            case '^':
+                continue;
+            case '&':
+                continue;
+            case '*':
+                continue;
+            case '(':
+                continue;
+            case ')':
+                continue;
+            case '+':
+                continue;
+            default:
+                cont1.innerHTML += x
+                break;
+            
+        }
+    }
+
+    
 }
+//resetting form on alerts exit\/
+const form = document.querySelector('form')
 function exitalert(clicked) {
     modal.style.display = 'none'
+    form.reset()
 }
+
 // underlining the about page
 function aler() {
     alert("uiwhfiuwehf")
