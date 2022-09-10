b022f28fa9aa3b57a86e4e2f74120f8df8deb2f8
JoshuaFaaui <joshuafaaui@gmail.com>
2022-09-10 22:18:03 +1200

    changed up the email

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ const usern = document.getElementById("name")
 const usernum = document.getElementById("number")
 const cont = document.querySelector('.cont')
 const cont1 = document.getElementById("cont1")
+
 //---------function runs on submition\/
-function submitter(clicked) {
+function submitter() {
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "kickem4life@gmail.com",
+        Password: "4A731475388AD51380DD1D2A004904D0F050",
+        To: "kickem4life@gmail.com",
+        From: userem.value,
+        Subject: "My Enquiry For KWS SHOES",
+        Body: usermess.value
+    }).then((success) => {
+        console.log("message sent successfully")
+    }).catch((error) => {
+        console.log("message failed to send")
+    })
+    //--------------\/if theres no value in the inputs, the code wont run\/
     if (usermess.value.length && userem.value.length > 0) {
         if(usern.value.length && usernum.value.length > 0){
             modal.style.display = 'block';
@@ -52,6 +68,7 @@ function submitter(clicked) {
     }
     
 }
+
 //resetting form on alerts exit\/
 const form = document.querySelector('form')
 function exitalert(clicked) {
